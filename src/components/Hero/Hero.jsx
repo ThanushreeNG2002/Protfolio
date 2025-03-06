cf4009738bcb2252a310b79cb6f8f7bb4f172feb
SanthoshKumar9618 <santhoshbhavya2001@gmail.com>
2025-03-06 22:03:30 +0530

    first commit
    git push origin main

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+//import { getImageUrl } from "../../utils";
+import heroImage from "../../assets/hero/heroImage.png";
+
+
+
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Thanushree N G</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      {/* <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      /> */}
+      <img src={heroImage} alt="Hero image" className={styles.heroImg} />;
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
